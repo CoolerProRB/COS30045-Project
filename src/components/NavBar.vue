f91194cd473eb56d62b117b1deaeb3b713bc26e2
CoolerProYT <veronlam1818@gmail.com>
2024-10-04 19:06:45 +0800

    Deploy to GitHub Pages

---
+++ src/components/NavBar.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+</script>
+
+<template>
+    <nav>
+        <ul>
+            <li><router-link to="/">Home</router-link></li>
+            <li><router-link to="/about">About</router-link></li>
+            <li><router-link to="/contact">Contact</router-link></li>
+        </ul>
+
+        <div class="min-h-screen p-4">
+            <button @click="toggleTheme" class="py-2 px-4 mb-4 rounded bg-blue-500 text-white">
+                Toggle Dark Mode
+            </button>
+        </div>
+    </nav>
+</template>
+
+<style scoped>
+
+</style>
