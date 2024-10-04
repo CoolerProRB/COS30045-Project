0dc878619951233d35a58d82c2178e7c73780563
CoolerProYT <veronlam1818@gmail.com>
2024-10-04 16:48:49 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# COS30045-Project
+
+This is a group project for COS30045 - Data Visualization at Swinburne University of Technology.
+
+This project is developed using Vue3, Tailwind and D3.js.
---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="bg-blue-500 text-white p-4">
+    <h1 class="text-3xl font-bold">Hello, Tailwind CSS with Vue!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style></style>
