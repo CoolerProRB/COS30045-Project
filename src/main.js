b7fead3e59129d930fa2663edb05d9bc852bdf79
CoolerProYT <veronlam1818@gmail.com>
2024-10-04 19:49:45 +0800

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 import App from './App.vue'
 import Home from './components/Home.vue'
