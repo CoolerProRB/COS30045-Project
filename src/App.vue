ca23cdabb7f00fa2f9f52ac8cfee7d1a4d8de4d4
CoolerProYT <veronlam1818@gmail.com>
2024-10-20 15:04:08 +0800

    Updated footer text

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
 
     <footer class="flex flex-col gap-2 py-6 w-full shrink-0 items-center px-4 md:px-6 border-t border-gray-200 dark:border-gray-800">
         <p class="text-xs text-gray-500 dark:text-gray-400">
-            © {{ new Date().getFullYear() }} Cancer Data Visualization. All rights reserved.
+            © {{ new Date().getFullYear() }} Analysis of Respiratory Cancer. All rights reserved.
         </p>
     </footer>
 </template>
