870200c5fec898825ade59774ab3583bc8358002
CoolerProYT <j22036431@student.newinti.edu.my>
2024-11-01 18:44:09 +0800

    removed unused route

--- src/main.js
+++ src/main.js
@@ -3,20 +3,10 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 
 import App from './App.vue'
 import Home from './components/Home.vue'
-import BarChart from './components/BarChart.vue'
-import MapChart from "@/components/MapChart.vue";
-import StackedBarChart from "@/components/StackedBarChart.vue";
-import LineChart from "@/components/LineChart.vue";
-import Heatmap from "@/components/Heatmap.vue";
 import './assets/index.css'
 
 const routes = [
     { path: '/', component: Home },
-    { path: '/bar', component: BarChart },
-    { path: '/map', component: MapChart },
-    { path: '/stacked-bar', component: StackedBarChart },
-    { path: '/line', component: LineChart },
-    { path: '/heatmap', component: Heatmap },
     { path: '/:pathMatch(.*)*', redirect: '/' },
 ];
 
